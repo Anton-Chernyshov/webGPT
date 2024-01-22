b0976dd4bfbd653387d9c4a441cc00c4ca393436
Anton Chernyshov <116961964+Anton-Chernyshov@users.noreply.github.com>
2024-01-22 22:11:53 +0000

    ADDS basic functionality and gui

---
+++ script.js
@@ -0,0 +1,10 @@
+function closeSideMenu(){
+    document.getElementById("sideMenu").style.width = "0px";
+    
+}
+
+function openSideMenu(){
+
+    document.getElementById("sideMenu").style.width = "250px";
+
+}
