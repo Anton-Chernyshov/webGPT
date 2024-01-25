5f325aea7cf20903f2ab8fac0edbc56b9c2ecce6
Anton-Chernyshov <21chernyshova@tonbridge-school.org>
2024-01-25 23:13:14 +0000

    chagned filepaths

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 
-const htmlMain = "/home/pi/webGPT/frontend/webGPT/index.html";
-const cssMain = "/home/pi/webGPT/frontend/webGPT/style.css";
-const jsMain = "/home/pi/webGPT/frontend/webGPT/script.js";
+const htmlMain = "/home/pi/webGPT/WEBSERVER/webGPT/Frontend/index.html";
+const cssMain = "/home/pi/webGPT/WEBSERVER/webGPT/Frontend/style.css";
+const jsMain = "/home/pi/webGPT/WEBSERVER/webGPT/Frontend/script.js";
 const port = 8080;
 const app = express();
 
